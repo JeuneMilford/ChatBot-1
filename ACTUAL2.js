49459f3030d4d72c6e7df3bccb400ffb223e8de5
JeuneMilford <64503693+JeuneMilford@users.noreply.github.com>
2020-06-12 10:57:13 +1200

    Update ACTUAL2.js
    Updated changes using function response

--- ACTUAL2.js
+++ ACTUAL2.js
@@ -10,32 +10,24 @@ submit.addEventListener("click", response);
     
 const greetings = ["HeLlo" , "WhAt's Up" , "WhAt'S pOpPiN"]
 
-const botgreetings = Math.floor(Math.random() * 4);
 
-function response() {
-    while(botchat.textContent == "WhAt Is YoUr NaMe?") {
-        botchat.textContent = greetings[botgreetings] + " " + userInput.value;
-        setTimeout(() => {botchat.textContent = "HoW wAs YoUr DaY? GoOd or BaD"}, 2000)
-    }
+const botgreetings = Math.floor(Math.random() * 2);
 
-    while(botchat.textContent == "HoW wAs YoUr DaY? GoOd or BaD") {
-        if (userInput.value == "Good" ) {
-            botchat.textContent = "ThAtS fAnTaStIc";
-            
-        }
-        else if (userInput.value == "Bad") {
-            botchat.textContent = "Oh MaN, tOmOrRoW wIlL bE bEtTeR";
-        }
-        else {
-            botchat.textContent = "Please type GoOd or BaD"
-        }
-    while(botchat.textContent= "Please type GoOd or BaD") {
+function response () {
+    botchat.textContent = greetings[botgreetings] + " " + userInput.value + " " + "hOw WaS yOuR dAy? GoOd or BaD";
+    submit.addEventListener("click" , nextQuestion);
+   
 
-    }
-
-
-    }
-    
-    
+} 
 
     
+function nextQuestion () {
+    botchat.textContent = "er try use Bad or Good?";
+    if ( userInput.value == "Good" ) {
+        botchat.textContent = "YaY, tHaT's GoOd To HeAr,ClIcK tO sEe NeXt QuEsTiOn";
+        submit.addEventListener("click" , nextResponse);
+    }
+    if (userInput.value == "Bad") {
+        botchat.textContent = "PiTy, ClIcK tO SeE iF tHe NeXt QuEsTiOn WiLl ChEeR yOu Up";
+        submit.addEventListener("click" , nextResponse);
+    }
